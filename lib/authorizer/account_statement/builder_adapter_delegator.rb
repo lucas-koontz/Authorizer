ac14722a35b899d419a3e9ad5d0d3f7fbbaeec26
Lucas Koontz <lucas.emanuel19@gmail.com>
2021-02-15 03:14:28 -0300

    add: builder delegator

---
+++ lib/authorizer/account_statement/builder_adapter_delegator.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Authorizer
+  module AccountStatement
+    class BuilderAdapterDelegator
+      attr_reader :event
+  
+      def initialize(event)
+        @event = event
+      end
+  
+      def adapter
+        return Adapters::CreationStatementAdapter if event[:account]
+        return Adapters::TransactionStatementAdapter if event[:transaction]
+      end
+    end
+  end
+end
---
+++ spec/authorizer/account_statement/builder_adapter_delegator_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+
+RSpec.describe Authorizer::AccountStatement::BuilderAdapterDelegator do
+
+  let(:creation_event) do
+    { account: { "active-card": true, "available-limit": 100 } }
+  end
+
+  let(:transaction_event) do
+    { transaction: { merchant: 'Burger King', amount: 20, time: '2019-02-13T10:00:00.000Z' } }
+  end
+
+  describe "#adapter" do
+    it 'can delegate to CreationStatement adapter' do
+      expect(described_class.new(creation_event).adapter).to eq Authorizer::AccountStatement::Adapters::CreationStatementAdapter
+    end
+
+    it 'can delegate to TransactionStatement adapter' do
+      expect(described_class.new(transaction_event).adapter).to eq Authorizer::AccountStatement::Adapters::TransactionStatementAdapter
+    end
+  end
+
+end
