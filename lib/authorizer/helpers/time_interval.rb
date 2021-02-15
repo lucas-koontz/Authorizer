c987f478e0c5adc6bbdfd292b78391831291bf7a
Lucas Koontz <lucas.emanuel19@gmail.com>
2021-02-15 07:09:54 -0300

    add: time interval helper

---
+++ lib/authorizer/helpers/time_interval.rb
@@ -0,0 +1,11 @@
+module Authorizer
+  module Helpers
+    module TimeInterval
+      class << self
+        def in_minutes_interval?(start_time:, end_time:, interval:)
+          interval.minutes.before(end_time) <= start_time
+        end
+      end
+    end
+  end
+end
